42ad37ff5fabcc5f0a0e9005339c5d20fe3c2706
zhangyu <l>
2018-10-29 17:52:07 +0800

    优化页面布局

--- pages/consilia/consilia.js
+++ pages/consilia/consilia.js
@@ -1,23 +1,21 @@
+var networkUtil = require("../common/js/network.js");
 Page({
   data: {
+    hidden: true
   },
   onLoad: function (option) {
     var that = this;
     var patientConditionId = option.patientConditionId;
     var req = { "content": { "patientConditionId": patientConditionId }, "os": "Android", "phone": "15311496135", "version": "V1.0" };
-    wx.request({
-      url: 'https://www.rzit.top/grape/patient/getConsiliaDetail',
-      header: {
-        contentType: 'application/json;charset=utf-8',
-      },
-      dataType: 'json',
-      method: "POST",
-      data: req,
-      success: function (res) {
-        console.log(res.data);
-        that.setData({ consilia: res.data.content });
-      }
-    })
+    networkUtil.postJson("https://www.rzit.top/grape/patient/getConsiliaDetail", req, "正在加载...", that.onGetConsiliaDetailSuccess, that.onGetConsiliaDetailFail);
+  },
+  onGetConsiliaDetailSuccess: function (data, requestCode) {
+    var that = this;
+    that.setData({ hidden: false });
+    that.setData({ consilia: data.content });
+  },
+  onGetConsiliaDetailFail: function (data, requestCode) {
+    var that = this;
   }
 })
 
