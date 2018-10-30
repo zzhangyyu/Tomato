5040a7884a800d849378ddd52960b065e62b0ab2
zhangyu <l>
2018-10-30 14:19:53 +0800

    增加导航背景、修改页面结构

--- pages/tabBar/nameDirBak.js
+++ pages/tabBar/nameDirBak.js
@@ -1,6 +1,13 @@
 var networkUtil = require("../common/js/network.js");
 Page({
   data: {
+    // 当前选择的导航字母
+    selected: 0,
+    // 选择字母视图滚动的位置id
+    scrollIntoView: 'A',
+    // 导航字母
+    letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+      'U', 'V', 'W', 'X', 'Y', 'Z'],
     pageIdx: 1,
     patientNameList: [],
     isLastPage: false
@@ -9,6 +16,32 @@ Page({
     var that = this;
     var reqJson = { "content": { "pageIdx": "1", "recordPerPage": getApp().globalData.recordPerPage}, "os": "Android", "phone": "15311496135", "version": "V1.0" };
     networkUtil.postJson("https://www.rzit.top/grape/patient/getConsiliaNameDir", reqJson, "正在加载...", that.onGetConsiliaNameDirSuccess, that.onGetConsiliaNameDirFail);
+
+
+
+    const res = wx.getSystemInfoSync(),
+      letters = this.data.letters;
+    // 设备信息
+    this.setData({
+      windowHeight: res.windowHeight,
+      windowWidth: res.windowWidth,
+      pixelRatio: res.pixelRatio
+    });
+    const navHeight = this.data.windowHeight * 0.94, // 
+      eachLetterHeight = navHeight / 26,
+      comTop = (this.data.windowHeight - navHeight) / 2,
+      temp = [];
+    this.setData({
+      eachLetterHeight: eachLetterHeight
+    });
+    for (let i = 0, len = letters.length; i < len; i++) {
+      const x = this.data.windowWidth - (10 + 50) / this.data.pixelRatio,
+        y = comTop + (i * eachLetterHeight);
+      temp.push([x, y]);
+    }
+    this.setData({
+      lettersPosition: temp
+    })
 
   },
   onPullDownRefresh: function () {
@@ -53,6 +86,7 @@ Page({
       curPatientNameList.push(internetPatientNameListData[i]);
     }
     that.setData({ patientNameList: curPatientNameList });
+
   },
   onLoadMoreConsiliaNameDirFail: function (data, requestCode) {
     var that = this;
@@ -64,6 +98,7 @@ Page({
     var reqJson = { "content": { "patientNameLike": patientNameLike, "pageIdx": "1", "recordPerPage": getApp().globalData.recordPerPage}, "os": "Android", "phone": "15311496135", "version": "V1.0" };
     networkUtil.postJson("https://www.rzit.top/grape/patient/getConsiliaNameDir", reqJson, "正在加载...", that.onGetConsiliaNameDirSuccess, that.onGetConsiliaNameDirFail);
   },
+
   searchValueInput: function (e) {
     var value = e.detail.value;
     this.setData({
@@ -76,5 +111,49 @@ Page({
       networkUtil.postJson("https://www.rzit.top/grape/patient/getConsiliaNameDir", reqJson, "正在加载...", that.onGetConsiliaNameDirSuccess, that.onGetConsiliaNameDirFail);
     }
   },
+
+  tabLetter(e) {
+    const index = e.currentTarget.dataset.index;
+    console.log(index);
+    this.setData({
+      selected: index,
+      scrollIntoView: index
+    })
+    this.cleanAcitvedStatus();
+  },
+  // 清除字母选中状态
+  cleanAcitvedStatus() {
+    setTimeout(() => {
+      this.setData({
+        selected: 0
+      })
+    }, 1000);
+  },
+  touchmove(e) {
+    const x = e.touches[0].clientX,
+      y = e.touches[0].clientY,
+      lettersPosition = this.data.lettersPosition,
+      eachLetterHeight = this.data.eachLetterHeight,
+      letters = this.data.letters;
+    console.log(y);
+    // 判断触摸点是否在字母导航栏上
+    if (x >= lettersPosition[0][0]) {
+      for (let i = 0, len = lettersPosition.length; i < len; i++) {
+        // 判断落在哪个字母区域，取出对应字母所在数组的索引，根据索引更新selected及scroll-into-view的值
+        const _y = lettersPosition[i][1], // 单个字母所处高度
+          __y = _y + eachLetterHeight; // 单个字母最大高度取值范围， 50为字母高50rpx
+        if (y >= _y && y <= __y) {
+          this.setData({
+            selected: letters[i],
+            scrollIntoView: letters[i]
+          });
+          break;
+        }
+      }
+    }
+  },
+  touchend(e) {
+    this.cleanAcitvedStatus();
+  }
 })
 
